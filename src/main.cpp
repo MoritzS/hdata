ce3cfb04e66c2eeed2b2bdc113f1923675f00e6d
Moritz Sichert <sichert@in.tum.de>
2014-12-03 17:18:28 +0100

    Added arg parsing for command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,81 @@
+#include <config.h>
+
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <string>
+#include <vector>
+
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <tclap/CmdLine.h>
 
 #include "bptree.h"
 #include "locations.h"
 
 #define MAX_LOCATIONS 4000000
 
+
+char const MODE_ADJ[] = "adj";
+char const MODE_NI[] = "ni";
+char const MODE_DELTANI[] = "deltani";
+
+std::vector<std::string> MODES = {MODE_ADJ, MODE_NI, MODE_DELTANI};
+
+
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        printf("usage: %s filename\n", argv[0]);
+    TCLAP::CmdLine args(PACKAGE_STRING);
+    TCLAP::ValueArg<std::string> locsArg(
+        "l", "locations",
+        "Path to locations.tbl file",
+        true,
+        "",
+        "file path"
+    );
+    TCLAP::ValuesConstraint<std::string> modeConstraint(MODES);
+    TCLAP::UnlabeledValueArg<std::string> modeArg(
+        "mode",
+        "Mode of execution",
+        true,
+        "",
+        &modeConstraint
+    );
+
+    args.add(locsArg);
+    args.add(modeArg);
+
+    args.parse(argc, argv);
+
+    std::string mode = modeArg.getValue();
+
+    if (mode != MODE_ADJ) {
+        fprintf(stderr, "Only \"adj\" mode is implemented!\n");
         return 1;
     }
+
+    printf("Running in \"%s\" mode\n", mode.c_str());
+
     Location* locs = (Location*)calloc(MAX_LOCATIONS, sizeof(Location));
     if (locs == nullptr) {
-        printf("couldn't allocate memory\n");
+        fprintf(stderr, "couldn't allocate memory\n");
         return 1;
     }
-    char* filename = argv[1];
+    char const* filename = locsArg.getValue().c_str();
     BPTree locs_tree;
+    printf("reading locations... ");
     FILE* locs_file = fopen(filename, "r");
     size_t num_locs = read_locations(locs_file, MAX_LOCATIONS, locs);
+    fclose(locs_file);
     if (num_locs == 0) {
-        printf("couldn't read locations\n");
+        printf("error\n");
         free(locs);
         return 1;
     }
     for (size_t i=0; i<num_locs; i++) {
         locs_tree.insert((int32_t)locs[i].id, locs+i);
     }
-    printf("read %zi locations\n", num_locs);
-    printf("depth: %zi\n", locs_tree.depth());
+    printf("got %zi\n", num_locs);
     char *line;
     while(1) {
         line = readline("> ");
